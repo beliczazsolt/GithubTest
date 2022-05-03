674c15c7623f3572739b2ffffdbcb4bb9a8aa021
Zsolt Belicza <zsolt.belicza@bt.com>
2022-05-03 22:16:56 +0200

    print updated

--- GithubTest/src/com/bt/demo/FirstClass.java
+++ GithubTest/src/com/bt/demo/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Welcome from the github demo");
+		System.out.println("Welcome from the github demo2");
 	}
 
 }
